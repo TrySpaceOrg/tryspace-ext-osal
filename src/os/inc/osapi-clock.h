ea4b7b01222db0e153cef555132a290875a9c267
Lucas, John P. <John.P.Lucas@tryspace.org>
2025-09-15 18:08:13 -0400

    [tryspaceorg/tryspace-lab#51] Modifications after CompFlags.cmake improvements;

--- src/os/inc/osapi-clock.h
+++ src/os/inc/osapi-clock.h
@@ -575,7 +575,8 @@ static inline bool OS_TimeEqual(OS_time_t time1, OS_time_t time2)
  */
 static inline int8_t OS_TimeGetSign(OS_time_t time)
 {
-    return (time.ticks > 0) - (time.ticks < 0);
+    int sign = (time.ticks > 0) - (time.ticks < 0);
+    return (int8_t)sign;
 }
 
 /*-------------------------------------------------------------------------------------*/
